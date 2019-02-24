9d02d4ed8fb878f785d06f6dfb6d144114456e62
Hugo Da Roit <contact@hdaroit.fr>
2019-02-24 23:36:53 +0100

    Fix coins

--- include/RandomHelpers.h
+++ include/RandomHelpers.h
@@ -4,6 +4,6 @@
 static int getRandomNumber(int start, int end) {
     std::random_device rd;
     std::mt19937 eng(rd());
-    std::uniform_int_distribution<> distr(start, end);
+    std::uniform_int_distribution<int> distr(start, end);
     return distr(eng);
 }
